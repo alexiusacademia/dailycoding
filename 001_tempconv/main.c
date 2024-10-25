e98ee2a94283f5efec624879fd90410d8ff2f81d
alexiusacademia <alexius.sayco.academia@gmail.com>
2024-10-25 21:32:34 +0800

    Initial commit

---
+++ 001_tempconv/main.c
@@ -0,0 +1,26 @@
+/*
+    001_tempconv/main.c
+
+    A simple program that converts temperature in Celcius 
+    to Fahrenheit.
+
+    Author: syncster.dev
+    Date:   October 25, 2024
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float cel;                  // Input temperature in Celcius
+    float fah;                  // Output in Fahrenheit
+
+    printf("Enter temperature in Celcius: ");
+    scanf("%f", &cel);
+
+    fah = cel * 9.0 / 5 + 32;   // Formulat to convert celcius to fahrenheit
+
+    printf("Temperature in fahrenheit is: %.2f\n", fah);
+
+    return 0;
+}
